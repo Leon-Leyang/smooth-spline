3d96ce4f7d6b189b170a2a532755111714c003e9
Leon-Leyang <leyang_hu@brown.edu>
2024-09-06 15:51:29 -0400

    Add smooth relu

--- utils.py
+++ utils.py
@@ -1,4 +1,5 @@
 import torch
+import torch.nn as nn
 
 
 def replacement(name, module):
@@ -21,3 +22,35 @@ def replace_module(model, replacement_mapping):
             parent = getattr(parent, name)
         setattr(parent, module_names[-1], replacement)
     return model
+
+
+class SmoothReLU(nn.Module):
+    def __init__(self, smoothness=0,in_features=1, trainable=False):
+        super().__init__()
+        param = torch.nn.Parameter(torch.zeros(in_features)+smoothness)
+        param.requires_grad_(trainable)
+        self.register_parameter("smoothness", param)
+
+    def forward(self, x):
+        return torch.sigmoid(x / nn.functional.softplus(self.smoothness)) * x
+
+class LazySmoothReLU(nn.modules.lazy.LazyModuleMixin, SmoothReLU):
+
+    cls_to_become = SmoothReLU
+    weight: nn.parameter.UninitializedParameter
+
+    def __init__(self, axis=-1, device=None, dtype=None):
+        super().__init__()
+        if type(axis) not in [tuple, list]:
+            axis = [axis]
+        self.axis = axis
+        self.smoothness = nn.parameter.UninitializedParameter(device=device, dtype=dtype)
+
+    def initialize_parameters(self, input) -> None:
+        if self.has_uninitialized_params():
+            with torch.no_grad():
+                s = [1 for _ in range(input.ndim)]
+                for i in self.axis:
+                    s[i] = input.size(i)
+                self.smoothness.materialize(s)
+                self.smoothness.copy_(torch.Tensor([0.541323]))
