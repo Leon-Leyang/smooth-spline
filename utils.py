31f0d720cb6381741123110a1c96d8070549fdb7
Leon-Leyang <leyang_hu@brown.edu>
2024-09-09 11:43:01 -0400

    Add `Network`

--- utils.py
+++ utils.py
@@ -60,3 +60,28 @@ class LazySmoothReLU(nn.modules.lazy.LazyModuleMixin, SmoothReLU):
                     s[i] = input.size(i)
                 self.smoothness.materialize(s)
                 self.smoothness.copy_(torch.Tensor([self.val_smoothness]))
+
+
+class Network(nn.Module):
+    """
+    A simple neural network for binary classification.
+    """
+    def __init__(self, in_features: int, depth: int, width: int, nonlinearity: nn.Module):
+        super().__init__()
+        self.register_buffer("depth", torch.as_tensor(depth))
+        self.layer0 = torch.nn.Linear(in_features, width)
+        for i in range(1, depth):
+            setattr(
+                self,
+                f"layer{i}",
+                nn.Linear(width, width),
+            )
+        self.output_layer = nn.Linear(width, 1)
+        self.nonlinearity = nonlinearity
+
+    def forward(self, x):
+        for i in range(self.depth):
+            x = getattr(self, f"layer{i}")(x)
+            x = self.nonlinearity(x)
+        x = self.output_layer(x)
+        return x
