2b86cebb1a130844c9e185bf46225957c37988f0
Leon-Leyang <leyang_hu@brown.edu>
2024-09-16 20:12:18 -0400

    Remove data augmentation

--- cifar100_to_cifar10.py
+++ cifar100_to_cifar10.py
@@ -21,8 +21,6 @@ device = 'cuda' if torch.cuda.is_available() else 'cpu'
 
 # Data Preparation
 transform_train = transforms.Compose([
-    transforms.RandomCrop(32, padding=4),
-    transforms.RandomHorizontalFlip(),
     transforms.ToTensor(),
     transforms.Normalize((0.5071, 0.4867, 0.4408), (0.2675, 0.2565, 0.2761)),
 ])
