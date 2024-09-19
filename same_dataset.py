5c7de9a62f4bb823ba85c7e3c0af6477dd4598f2
Leon-Leyang <leyang_hu@brown.edu>
2024-09-19 12:06:02 -0400

    Add test for `overfit`

--- same_dataset.py
+++ same_dataset.py
@@ -8,14 +8,13 @@ device = 'cuda' if torch.cuda.is_available() else 'cpu'
 
 
 def main():
+    # Replace ReLU with BetaReLU and test the model on the original dataset
     mode = 'normal'
     dataset = 'cifar100'
     ckpt_folder = os.path.join('./ckpts', mode)
     model = resnet18().to(device)
     model.load_state_dict(torch.load(os.path.join(ckpt_folder, f'resnet18_{dataset}_epoch200.pth'), weights_only=True))
     _, test_loader = get_data_loaders(dataset, 128)
-
-    # Replace ReLU with BetaReLU and test the model on the original dataset
     beta_vals = np.arange(0.9999, 1, 0.000002)
     replace_and_test(model, test_loader, beta_vals, mode, dataset, __file__)
 
@@ -23,6 +22,12 @@ def main():
     ckpt_folder = os.path.join('./ckpts', mode)
     model.load_state_dict(torch.load(os.path.join(ckpt_folder, f'resnet18_{dataset}_epoch200.pth'), weights_only=True))
     beta_vals = np.arange(0.975, 1, 0.0005)
+    replace_and_test(model, test_loader, beta_vals, mode, dataset, __file__)
+
+    mode = 'overfit'
+    ckpt_folder = os.path.join('./ckpts', mode)
+    model.load_state_dict(torch.load(os.path.join(ckpt_folder, f'resnet18_{dataset}_epoch200.pth'), weights_only=True))
+    beta_vals = np.arange(0.9, 1, 0.1)
     replace_and_test(model, test_loader, beta_vals, mode, dataset, __file__)
 
 
