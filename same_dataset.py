7d56fc3befced0460a67bae5eb18af199b623688
Leon-Leyang <leyang_hu@brown.edu>
2024-09-19 14:00:41 -0400

    Add same dataset test for cifar10

--- same_dataset.py
+++ same_dataset.py
@@ -7,53 +7,32 @@ from resnet import resnet18
 device = 'cuda' if torch.cuda.is_available() else 'cpu'
 
 
+def replace_and_test_on(mode, dataset, test_loader, model, beta_vals):
+    ckpt_folder = os.path.join('./ckpts', mode)
+    model.load_state_dict(torch.load(os.path.join(ckpt_folder, f'resnet18_{dataset}_epoch200.pth'), weights_only=True))
+    replace_and_test(model, test_loader, beta_vals, mode, dataset, __file__)
+
+
 def main():
-    # Replace ReLU with BetaReLU and test the model on CIFAR-100
-    mode = 'normal'
+    model = resnet18().to(device)
+
+    # Replace ReLU with BetaReLU and test the model trained on different conditions on CIFAR-100
     dataset = 'cifar100'
-    ckpt_folder = os.path.join('./ckpts', mode)
-    model = resnet18().to(device)
-    model.load_state_dict(torch.load(os.path.join(ckpt_folder, f'resnet18_{dataset}_epoch200.pth'), weights_only=True))
     _, test_loader = get_data_loaders(dataset, 128)
-    beta_vals = np.arange(0.9999, 1, 0.000002)
-    replace_and_test(model, test_loader, beta_vals, mode, dataset, __file__)
+    mode_2_beta_vals = {
+        'normal': np.arange(0.9999, 1, 0.000002),
+        'suboptimal': np.arange(0.975, 1, 0.0005),
+        'overfit': np.arange(0.9999, 1, 0.000002)
+    }
+    for mode, beta_vals in mode_2_beta_vals.items():
+        replace_and_test_on(mode, dataset, test_loader, model, beta_vals)
 
-    mode = 'suboptimal'
-    ckpt_folder = os.path.join('./ckpts', mode)
-    model.load_state_dict(torch.load(os.path.join(ckpt_folder, f'resnet18_{dataset}_epoch200.pth'), weights_only=True))
-    beta_vals = np.arange(0.975, 1, 0.0005)
-    replace_and_test(model, test_loader, beta_vals, mode, dataset, __file__)
-
-    mode = 'overfit'
-    ckpt_folder = os.path.join('./ckpts', mode)
-    model.load_state_dict(torch.load(os.path.join(ckpt_folder, f'resnet18_{dataset}_epoch200.pth'), weights_only=True))
-    beta_vals = np.arange(0.9999, 1, 0.000002)
-    replace_and_test(model, test_loader, beta_vals, mode, dataset, __file__)
-
-    # Replace ReLU with BetaReLU and test the model on CIFAR-10
-    mode = 'normal'
+    # Replace ReLU with BetaReLU and test the model on different conditions on CIFAR-10
     dataset = 'cifar10'
-    ckpt_folder = os.path.join('./ckpts', mode)
-    model = resnet18().to(device)
-    model.load_state_dict(torch.load(os.path.join(ckpt_folder, f'resnet18_{dataset}_epoch200.pth'), weights_only=True))
     _, test_loader = get_data_loaders(dataset, 128)
-    beta_vals = np.arange(0.9999, 1, 0.000002)
-    replace_and_test(model, test_loader, beta_vals, mode, dataset, __file__)
-
-    mode = 'suboptimal'
-    ckpt_folder = os.path.join('./ckpts', mode)
-    model.load_state_dict(torch.load(os.path.join(ckpt_folder, f'resnet18_{dataset}_epoch200.pth'), weights_only=True))
-    beta_vals = np.arange(0.975, 1, 0.0005)
-    replace_and_test(model, test_loader, beta_vals, mode, dataset, __file__)
-
-    mode = 'overfit'
-    ckpt_folder = os.path.join('./ckpts', mode)
-    model.load_state_dict(torch.load(os.path.join(ckpt_folder, f'resnet18_{dataset}_epoch200.pth'), weights_only=True))
-    beta_vals = np.arange(0.9999, 1, 0.000002)
-    replace_and_test(model, test_loader, beta_vals, mode, dataset, __file__)
+    for mode, beta_vals in mode_2_beta_vals.items():
+        replace_and_test_on(mode, dataset, test_loader, model, beta_vals)
 
 
 if __name__ == '__main__':
     main()
-
-
