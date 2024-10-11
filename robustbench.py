cb678535e3023fa699bf5e060cf46b2d496f7e93
Leon-Leyang <leyang_hu@brown.edu>
2024-10-10 20:38:49 -0400

    Expose `batch_size` as param

--- robustbench.py
+++ robustbench.py
@@ -4,8 +4,7 @@ from robustbench.utils import load_model
 from utils import replace_and_test_robustness
 
 
-def replace_and_test_robustness_on(model_id, threat, beta_vals, dataset, n_examples):
-    batch_size = 70
+def replace_and_test_robustness_on(model_id, threat, beta_vals, dataset, n_examples, batch_size):
     mode = 'normal'
     model = load_model(model_name=model_id, dataset=dataset, threat_model=threat)
     replace_and_test_robustness(model, threat, beta_vals, mode, dataset, __file__, batch_size=batch_size, n_examples=n_examples, transform_test=None, model_id=model_id)
@@ -16,15 +15,17 @@ def main():
     parser.add_argument('--model_ids', type=str, nargs='+', default=['Peng2023Robust', 'Wang2023Better_WRN-70-16'], help='List of model IDs to test')
     parser.add_argument('--dataset', type=str, default='cifar10', help='Dataset to use')
     parser.add_argument('--n_examples', type=int, default=10000, help='Number of examples to use')
+    parser.add_argument('--batch_size', type=int, default=70, help='Batch size for testing')
     args = parser.parse_args()
 
     threat = 'Linf'
     beta_vals = np.arange(0.95, 1 - 1e-6, 0.01)
     dataset = args.dataset
     n_examples = args.n_examples
+    batch_size = args.batch_size
 
     for model_id in args.model_ids:
-        replace_and_test_robustness_on(model_id, threat, beta_vals, dataset, n_examples)
+        replace_and_test_robustness_on(model_id, threat, beta_vals, dataset, n_examples, batch_size)
 
 
 if __name__ == "__main__":
