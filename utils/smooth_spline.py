d8abffbd84fcc827fa304ec382ce2d472d2ae4bb
Leon-Leyang <leyang_hu@brown.edu>
2024-12-27 12:40:40 -0500

    Add `threshold` for stability

--- utils/smooth_spline.py
+++ utils/smooth_spline.py
@@ -13,12 +13,15 @@ class BetaAgg(nn.Module):
 
     def forward(self, x):
         beta = self.beta
-        single_ver = torch.sigmoid(beta * x / (1 - beta)) * x
-        agg_ver = (
+        normal_ver = (
             self.coeff * torch.sigmoid(beta * x / (1 - beta)) * x +
             (1 - self.coeff) * torch.log(1 + torch.exp(x / (1 - beta))) * (1 - beta)
         )
-        return torch.where(x / (1 - beta) <= self.threshold, agg_ver, single_ver)
+        overflow_ver = (
+            self.coeff * torch.sigmoid(beta * x / (1 - beta)) * x +
+            (1 - self.coeff) * x
+        )
+        return torch.where(x / (1 - beta) <= self.threshold, normal_ver, overflow_ver)
 
 
 class ReplacementMapping:
