5c4877f493173bd23280e2d58a2a6c6b940b13d2
Leon-Leyang <leyang_hu@brown.edu>
2025-01-02 20:10:58 -0500

    Change init model weight path

--- utils/semseg/model/resnet.py
+++ utils/semseg/model/resnet.py
@@ -196,7 +196,7 @@ def resnet50(pretrained=False, **kwargs):
     model = ResNet(Bottleneck, [3, 4, 6, 3], **kwargs)
     if pretrained:
         # model.load_state_dict(model_zoo.load_url(model_urls['resnet50']))
-        model_path = './diff_task/initmodel/resnet50_v2.pth'
+        model_path = './ckpts/diff_task/resnet50_v2.pth'
         model.load_state_dict(torch.load(model_path, weights_only=True), strict=False)
     return model
 
@@ -210,7 +210,7 @@ def resnet101(pretrained=False, **kwargs):
     model = ResNet(Bottleneck, [3, 4, 23, 3], **kwargs)
     if pretrained:
         # model.load_state_dict(model_zoo.load_url(model_urls['resnet101']))
-        model_path = './initmodel/resnet101_v2.pth'
+        model_path = './ckpts/diff_task/resnet101_v2.pth'
         model.load_state_dict(torch.load(model_path, weights_only=True), strict=False)
     return model
 
@@ -224,6 +224,6 @@ def resnet152(pretrained=False, **kwargs):
     model = ResNet(Bottleneck, [3, 8, 36, 3], **kwargs)
     if pretrained:
         # model.load_state_dict(model_zoo.load_url(model_urls['resnet152']))
-        model_path = './initmodel/resnet152_v2.pth'
+        model_path = './ckpts/diff_task/resnet152_v2.pth'
         model.load_state_dict(torch.load(model_path, weights_only=True), strict=False)
     return model
